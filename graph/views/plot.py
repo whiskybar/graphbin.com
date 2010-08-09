10d18330de774e1a6392e583946e5ca5d4ed721b
Jiri Barton <jbar@hosting4u.cz>
2010-08-09 22:26:59 +0200

    used the uploaded file to render the chart

--- graph/views/plot.py
+++ graph/views/plot.py
@@ -1,8 +1,10 @@
 import web
+import os.path
 from web import form
 from graph.settings import UPLOADS
 from graph.views.base import View
 
 class Draw(View):
     def get(self, filename):
-        return {'array': [(1, 10), (2, 20), (3, 15)]}
+        return {'array': [[float(number) for number in line.split()] \
+            for line in open(os.path.join(UPLOADS, filename))]}
