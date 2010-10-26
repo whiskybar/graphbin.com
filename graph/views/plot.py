7dc1b33633c409e70aeedef0e55f534d44224587
Jiri Barton <jbar@hosting4u.cz>
2010-10-26 21:24:09 +0200

    relaxed the input format

--- graph/views/plot.py
+++ graph/views/plot.py
@@ -6,5 +6,4 @@ from graph.views.base import View
 
 class Draw(View):
     def get(self, filename):
-        return {'array': [[float(number) for number in line.split()] \
-            for line in open(os.path.join(UPLOADS, filename))]}
+        return {'array': open(os.path.join(UPLOADS, filename)).read()}
--- graph/views/upload.py
+++ graph/views/upload.py
@@ -1,14 +1,32 @@
+import re
+import json
 from tempfile import NamedTemporaryFile
 from os.path import basename
+
 import web
 from web import form
+
 from graph.settings import UPLOADS
 from graph.views.base import View
 
+
 class Create(View):
+
+    split_re = re.compile('[^0-9.+-]*')
+
+    def process_input(self, f):
+        for line in f:
+            serie = filter(None, self.split_re.split(line))
+            try:
+                if len(serie) > 1:
+                    yield [float(serie[0]), float(serie[1])]
+                elif len(serie) == 1:
+                    yield float(serie[0])
+            except ValueError:
+                continue
+
     def post(self):
-        csv = NamedTemporaryFile(prefix='', dir=UPLOADS, delete=False)
-        csv.write(web.input()['csvfile'])
-        csv.close()
-        return '/%s/' % basename(csv.name)
- 
+        with NamedTemporaryFile(prefix='', dir=UPLOADS, delete=False) as f:
+            json.dump(list(self.process_input(web.input(csvfile={})['csvfile'].file)), f)
+        return '/%s/' % basename(f.name)
+
